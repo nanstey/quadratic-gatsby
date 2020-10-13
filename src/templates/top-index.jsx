e6eadaa2c08d2b97e88edd722e308837fa55149c
Noel Anstey <noel@checkfront.com>
2020-10-13 12:22:33 -0700

    remove clients section

--- src/templates/top-index.jsx
+++ src/templates/top-index.jsx
@@ -33,10 +33,6 @@ export const query = graphql`
         frontmatter {
           brand
           anchor
-          clients {
-            href
-            imageFileName
-          }
           content
           copyright
           header
