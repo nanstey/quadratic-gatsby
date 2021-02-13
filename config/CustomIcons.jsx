2af9c92f9505e974f865b3690e983b8fc7f97423
Noel Anstey <noel@checkfront.com>
2021-02-12 21:08:13 -0800

    turn 'Coworking Space' into 'Education Programs' in the Services section

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -1,7 +1,7 @@
 import makeFAIcon from "utils/makeFAIcon";
 
-import { faMicrophone, faLaptop, faVideo } from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone, faLaptopCode, faVideo } from "@fortawesome/free-solid-svg-icons";
 
 export const MicrophoneIcon = makeFAIcon(faMicrophone);
-export const LaptopIcon = makeFAIcon(faLaptop);
+export const LaptopIcon = makeFAIcon(faLaptopCode);
 export const VideoIcon = makeFAIcon(faVideo);
