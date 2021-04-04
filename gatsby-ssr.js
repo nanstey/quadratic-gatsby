b5929a65dcd66db6720d8112767e17afe6582291
Noel Anstey <noel@checkfront.com>
2021-04-04 15:51:47 -0700

    try adding ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react");
+const { Helmet } = require("react-helmet");
+
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setBodyAttributes }) => {
+  const helmet = Helmet.renderStatic();
+  setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  setBodyAttributes(helmet.bodyAttributes.toComponent());
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.base.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.link.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
+};
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  const order = ["title", "base", "meta", "link", "noscript", "script", "style"];
+
+  const sortedHeadComponents = headComponents
+    .slice(0)
+    .flat()
+    .sort((x, y) => {
+      return order.indexOf(x.type) - order.indexOf(y.type);
+    });
+
+  replaceHeadComponents(sortedHeadComponents);
+};
