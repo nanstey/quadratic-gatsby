2698dda7f7a1809590f98f7c79fcdb13f12f926d
Noel Anstey <noel@checkfront.com>
2021-04-19 20:10:17 -0700

    attempt to return checkfront API call

--- src/functions/checkfrontAPI.js
+++ src/functions/checkfrontAPI.js
@@ -1,6 +1,22 @@
+const axios = require("axios");
+
+const { GATSBY_API_BASE, GATSBY_API_KEY, GATSBY_API_SECRET } = process.env;
+
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World", event, context }),
-  };
+  return axios
+    .get(`${GATSBY_API_BASE}/item`, {
+      auth: { username: GATSBY_API_KEY, password: GATSBY_API_SECRET },
+    })
+    .then((res) => {
+      return {
+        statusCode: 200,
+        body: res,
+      };
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        body: err,
+      };
+    });
 };
