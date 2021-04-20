83139503e728f8067c62ba086595f454bb69b852
Noel Anstey <noel@checkfront.com>
2021-04-19 20:38:53 -0700

    try destructing data property from axios call

--- src/functions/checkfrontAPI.js
+++ src/functions/checkfrontAPI.js
@@ -3,20 +3,12 @@ const axios = require("axios");
 const { GATSBY_API_BASE, GATSBY_API_KEY, GATSBY_API_SECRET } = process.env;
 
 exports.handler = async function (event, context) {
-  return axios
-    .get(`${GATSBY_API_BASE}/item`, {
-      auth: { username: GATSBY_API_KEY, password: GATSBY_API_SECRET },
-    })
-    .then((res) => {
-      return {
-        statusCode: 200,
-        body: res,
-      };
-    })
-    .catch((err) => {
-      return {
-        statusCode: 500,
-        body: err,
-      };
-    });
+  const { data } = await axios.get(`${GATSBY_API_BASE}/item`, {
+    auth: { username: GATSBY_API_KEY, password: GATSBY_API_SECRET },
+  });
+
+  return {
+    statusCode: 200,
+    body: { data, event, context },
+  };
 };
