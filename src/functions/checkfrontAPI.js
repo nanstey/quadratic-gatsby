3a3f73811ab99379444db89b489b8b32c805c68e
Noel Anstey <noel@checkfront.com>
2021-04-19 21:03:17 -0700

    use query parameters to specify route on the fly

--- src/functions/checkfrontAPI.js
+++ src/functions/checkfrontAPI.js
@@ -3,12 +3,23 @@ const axios = require("axios");
 const { GATSBY_API_BASE, GATSBY_API_KEY, GATSBY_API_SECRET } = process.env;
 
 exports.handler = async function (event, context) {
-  const { data } = await axios.get(`${GATSBY_API_BASE}/item`, {
+  const endpoint = event.queryStringParameters.route;
+  delete event.queryStringParameters.route;
+
+  if (!endpoint) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify("You must provide a route in the query parameters"),
+    };
+  }
+
+  const { data } = await axios.get(`${GATSBY_API_BASE}/${endpoint}`, {
     auth: { username: GATSBY_API_KEY, password: GATSBY_API_SECRET },
+    params: event.queryStringParameters,
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ data, event, context }),
+    body: JSON.stringify(data),
   };
 };
