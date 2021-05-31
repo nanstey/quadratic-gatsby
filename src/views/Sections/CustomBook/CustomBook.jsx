be7fe49ab4071704f289b527178c1979f700f09d
Noel Anstey <noel@checkfront.com>
2021-05-30 21:58:11 -0700

    add age selection dropdown to make non matching items inactive, greyscale inactive items

--- src/views/Sections/CustomBook/CustomBook.jsx
+++ src/views/Sections/CustomBook/CustomBook.jsx
@@ -3,21 +3,39 @@ import PropTypes from "prop-types";
 
 import PageSection from "components/PageSection";
 import SectionHeader from "components/SectionHeader";
-import { Container, Row, Card } from "react-bootstrap";
+import { Container, Row, Card, Dropdown } from "react-bootstrap";
+import { range } from "underscore";
+
 import useItems from "../../../hooks/useItems";
 
 import "./CustomBook.scss";
 
 const CustomBook = () => {
   const items = useItems();
+  const [selectedAge, setSelectedAge] = useState(null);
 
   return (
     <PageSection id="CustomBook">
       <Row>
         <SectionHeader header="Pick a Camp" subheader="" />
         <Container className="course-container">
+          <div className="course-filters">
+            <Dropdown>
+              <Dropdown.Toggle className="dropdown-primary" variant="primary" id="dropdown-basic">
+                {selectedAge ? `Age: ${selectedAge}` : "Select Age"}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {range(6, 15).map((age) => (
+                  <Dropdown.Item key={age} onClick={() => setSelectedAge(age)}>
+                    {age}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
           {items.map((item) => {
-            return <CourseCard item={item} key={item.itemId} />;
+            const active = !selectedAge || (selectedAge >= item.ageMin && selectedAge <= item.ageMax);
+            return <CourseCard key={item.itemId} item={item} active={active} />;
           })}
         </Container>
       </Row>
@@ -27,10 +45,12 @@ const CustomBook = () => {
 
 export default CustomBook;
 
-const CourseCard = ({ item }) => {
+const CourseCard = ({ item, active }) => {
   const itemTooltip = item.available ? "Book Now" : "Join Waitlist";
   const baseUrl = "https://quadraticsound.checkfront.com/reserve?item_id=";
   const dateString = item.dateStart.format("YYYYMMDD");
+
+  const activeClass = active ? 'active' : 'inactive';
 
   const itemLink = item.available
     ? `${baseUrl}${item.itemId}`
@@ -39,7 +59,7 @@ const CourseCard = ({ item }) => {
   return (
     <div className="course-card-wrapper">
       <a target="_blank" rel="noreferrer" href={itemLink} title={itemTooltip}>
-        <Card className="course-card" key={item.itemId}>
+        <Card className={`course-card ${activeClass}`} key={item.itemId}>
           {!item.available && <span className="sold-out">SOLD OUT</span>}
           <Card.Img className="course-card-image" variant="top" src={item.image["1"].url} />
           <Card.Body className="course-card-body">
@@ -54,7 +74,6 @@ const CourseCard = ({ item }) => {
           <Card.Header className="course-card-header">
             <h5 className="course-card-title">{item.title}</h5>
           </Card.Header>
-
         </Card>
       </a>
     </div>
@@ -75,4 +94,5 @@ CourseCard.propTypes = {
     dateStart: PropTypes.object,
     dateEnd: PropTypes.object,
   }).isRequired,
+  active: PropTypes.bool.isRequired
 };
